397ce1174c63eb6c886e471ef05b6ad74373ca45
Aleks <johnhenk1122@gmail.com>
2018-02-07 21:15:40 +0100

    Propagacja wsteczna.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.nio.file.Path;
@@ -44,13 +45,17 @@ public class Main {
     public static ArrayList TD = new ArrayList<>(); // Testowe Dane wejściowe(uczące)
     public static ArrayList TDM = new ArrayList<>(); // Testowe Dane wyjsciowe (uczące)
     public static ArrayList Weight = new ArrayList<>(); // Wagi synaps
+    public static ArrayList WeightTMP = new ArrayList<>(); // Wagi synaps
+    public static ArrayList Actual = new ArrayList<>(); // Oczekiwania
     public static ArrayList HLOne = new ArrayList<>(); // Wyniki w pierwszej warstwie ukrytej
+    public static ArrayList Error = new ArrayList<>(); // Wyniki w pierwszej warstwie ukrytej
     public static ArrayList HLTwo = new ArrayList<>(); // Wyniki w drugiej warstwie ukrytej
     public static ArrayList TDOUT = new ArrayList<>(); // Dane wyjściowe po przeliczeniu
 
     public static int lengthOfLine = 0;
     public static int lOW = 0;//length of weights
-    public static int lOI = 1;//length of inputs
+    public static int lOI = 0;//length of inputs
+    public static double L = 0.5;
 
     public static Random generator = new Random();
 
@@ -72,12 +77,16 @@ public class Main {
         System.out.println(">>>------ Hidden Neurons: 6 ---------<<<");
         System.out.println(">>>------ Learning items: 75 --------<<<");
         System.out.println(">>>----------------------------------<<<");
-        System.out.println(">>Wyświetlić szczegóły uruchamiania? y/n");
+        System.out.println(">>>----Po 3 przykłady na 1 wyjście---<<<");
+        System.out.println(">>>----------------------------------<<<");
+        System.out.println(">>Wyświetlić szczegóły uruchamiania?");
+        System.out.println(">>Uwaga! Zajmie to sporo miejsca!");
+        System.out.println(">>Wpisz: y lub n");
         System.out.print(">");
-//
-//        answer = scan.next();
-//        if (answer.equals("y"))details = true;
-//        System.out.println();
+
+        answer = scan.next();
+        if (answer.equals("y"))details = true;
+        System.out.println();
 
 // Wczytanie pliku uczącego
         ReadDataFile();
@@ -91,7 +100,17 @@ public class Main {
 
 // Uczenie sieci
         Learning();
-        System.out.println(">>>-------------The End--------------<<<");
+        System.out.println(">>>----------------------------------<<<");
+        System.out.println(">>>-------- Postęp w uczeniu --------<<<");
+        System.out.println(">>>-------- w oparciu o błąd --------<<<");
+        System.out.println(">>>----------- na wyjściu -----------<<<");
+        System.out.println(">>>----------------------------------<<<");
+        for(int i = 0; i < Error.size()/3; i++) {
+        System.out.println(i + ") " + Error.get(i) + " -> " + Error.get(i+25) + " -> " + Error.get(i+50));
+        }
+        System.out.println(">>>----------------------------------<<<");
+        System.out.println(">>>--------Uczenie zakończone--------<<<");
+        System.out.println(">>>----------------------------------<<<");
     }
 
 
@@ -104,7 +123,26 @@ public class Main {
 */
     private static void Learning() {
         double path = 0;
-        int z = 0;
+        double stala = -0.95;
+
+        System.out.println(">>Generowanie wartości oczekiwanych...");
+
+        for (int i = 0; i<25; i++){
+            Actual.add(stala);
+            stala += 0.08;
+        }
+
+        for (int i = 0; i<50; i++){
+            Actual.add(Actual.get(i));
+        }
+        System.out.println(">Ukończono.");
+
+        if (details==true)checkActualList();
+
+
+        // Start Obliczeń i Propagacji wstecznej
+        for(int z = 0; z<TD.size()/4; z++) {
+            lOW = 0;
 
             System.out.println(">>>-------------Item " + z + "--------------<<<");
             //Pobranie danych wejściowych dla jednego ciągu uczącego (np: 1 2 0 1)
@@ -120,6 +158,7 @@ public class Main {
             int inputD = Integer.parseInt((String) TD.get(lOI + 3));
             lOI += 4;
 
+            System.out.println(">>Input: " + inputA + inputB + inputC + inputD);
             System.out.println(">>Obliczanie pierwszej warstwy ukrytej z 4 neuronami...");
             for (int i = 0; i < 4; i++) {
                 path = (inputA * (double) Weight.get(lOW)) + (inputB * (double) Weight.get(lOW + 1)) + (inputC * (double) Weight.get(lOW + 2)) + (inputD * (double) Weight.get(lOW + 3));
@@ -171,12 +210,217 @@ public class Main {
 
             System.out.println(">Ukończono.");
 
-//------- Propagacja wsteczna
+//------- Propagacja wsteczna - Obliczanie błędów
 
+            int weightTMP = lOW - 1;
+            System.out.println(">>Obliczanie propagacji wstecznej...");
+            double actual = (double) Actual.get(z);
+            System.out.println("Actual: " + actual);
+            double ErrorO = path * (1 - path) * (actual - path);
+            System.out.println("Error Out: " + ErrorO);
+            //--
+            double ErrorJ = inputJ * (1 - inputJ) * (ErrorO * (double) Weight.get(weightTMP));
+            weightTMP--;
+            System.out.println("Error J: " + ErrorJ);
+            //--
+            double ErrorI = inputJ * (1 - inputI) * (ErrorO * (double) Weight.get(weightTMP));
+            weightTMP--;
+            System.out.println("Error I: " + ErrorI);
+            //--
+            double ErrorH = inputH * (1 - inputH) * (((ErrorJ * (double) Weight.get(weightTMP)) + (ErrorI * (double) Weight.get(weightTMP - 1))) / 2);
+            weightTMP--;
+            weightTMP--;
+            System.out.println("Error H: " + ErrorH);
+            //--
+            double ErrorG = inputG * (1 - inputG) * (((ErrorJ * (double) Weight.get(weightTMP)) + (ErrorI * (double) Weight.get(weightTMP - 1))) / 2);
+            weightTMP--;
+            weightTMP--;
+            System.out.println("Error G: " + ErrorG);
+            //--
+            double ErrorF = inputF * (1 - inputF) * (((ErrorJ * (double) Weight.get(weightTMP)) + (ErrorI * (double) Weight.get(weightTMP - 1))) / 2);
+            weightTMP--;
+            weightTMP--;
+            System.out.println("Error F: " + ErrorF);
+            //--
+            double ErrorE = inputE * (1 - inputE) * (((ErrorJ * (double) Weight.get(weightTMP)) + (ErrorI * (double) Weight.get(weightTMP - 1))) / 2);
+            weightTMP--;
+            weightTMP--;
+            System.out.println("Error E: " + ErrorE);
 
+//------- Propagacja wsteczna - Zmiana wag
 
+            int lOWTmp = lOW;
+            double newWeight = 0;
+            double w = 0;
+            lOWTmp = lOWTmp - 26;
+            //-----dla E
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorE * inputA;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorE * inputB;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorE * inputC;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorE * inputD;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            //-----dla F
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorF * inputA;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorF * inputB;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorF * inputC;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorF * inputD;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            //-----dla G
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorG * inputA;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorG * inputB;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorG * inputC;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorG * inputD;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            //-----dla H
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorH * inputA;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorH * inputB;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorH * inputC;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorH * inputD;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            //---- dla I
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorI * inputE;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorI * inputF;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorI * inputG;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorI * inputH;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            //---- dla J
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorJ * inputE;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorJ * inputF;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorJ * inputG;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorJ * inputH;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            //----dla Output
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorO * inputI;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+            w = (double) Weight.get(lOWTmp);
+            newWeight = w + L * ErrorO * inputJ;
+            WeightTMP.add(newWeight);
+            lOWTmp++;
+            //--
+
+//                for (int i = 0; i < WeightTMP.size(); i++) {
+//                    System.out.println();
+//                    System.out.print(i + ") " + Weight.get(i) + "  ->  " + WeightTMP.get(i));
+//                }
+
+            Weight.clear();
+            for (int i = 0; i < WeightTMP.size(); i++) {
+                Weight.add(WeightTMP.get(i));
+            }
+            WeightTMP.clear();
+            Error.add(ErrorO);
+            System.out.println(">>Zmiana wagi...");
+            System.out.println(">Ukończono.");
+        }
     }
 
+    /*                          [ 01 - 2 ]
+|\      _,,,--,,_  ,)
+/,`.-'`'   -,  ;-;;'
+|,4-  ) )-,_ ) /\
+'---''(_/--' (_/-'
+>>>Check Actual list
+*/
+    private static void checkActualList() {
+        System.out.println(">>Sprawdzenie listy Actual...");
+        for (int i = 0; i<75; i++){
+            System.out.println(i + ")   " + Actual.get(i));
+        }
+        System.out.println(">Ukończono.");
+    }
 
 
     /*                          [ 02 ]
